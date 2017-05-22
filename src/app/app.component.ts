ebf8f111a2cf6fd1c309855d3c1d393c174fcb49
aethel <aethelbrihte@gmail.com>
2017-05-22 22:27:07 +0100

    background for welcome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Notches</h1>
+  template: `
+  <h1>Notches</h1>
   <router-outlet></router-outlet>
+
 	`,
 })
 export class AppComponent  { name = 'Angular'; }
