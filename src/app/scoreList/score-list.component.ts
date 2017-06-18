4019c8c38f460ac99450eb81ac90a03c02abd2a6
aethel <aethelbrihte@gmail.com>
2017-06-18 21:40:27 +0100

    list item scaling

--- src/app/scoreList/score-list.component.ts
+++ src/app/scoreList/score-list.component.ts
@@ -10,16 +10,26 @@ import {Router, ActivatedRoute} from '@angular/router';
 export class ScoreListComponent implements OnInit {
 	private setupObj:Object;
 	private playersArr:any[];
+  private orientation:any;
 
-	constructor(private _route: ActivatedRoute, private _router: Router) { }
+
+	constructor(private _route: ActivatedRoute, private _router: Router) {
+  }
 
 	ngOnInit():void {
 		this.setupObj = this._route.snapshot.params;
 		this.playersArr = Array(+this.setupObj['players']).fill(+this.setupObj['points']);
 		console.log(typeof(this.setupObj['players']),this.playersArr);
+    // this.orientationChange();
   }
 
   restart():void {
     this._router.navigate(['/welcome']);
   }
+
+  // orientationChange():void{
+  //   window.addEventListener('orientationchange', function(){
+  //     console.log(window.screen.orientation.type);
+  //   }, false);
+  // }
 }
